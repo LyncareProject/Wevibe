ce57639d2986ea5c9905908dc66dd834ab7a2b63
kun woo kim <kimkuns98@gmail.com>
2023-07-19 11:20:56 +0900

    백앤드 수정

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-
-
+const PORT = 8080
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,22 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
-
-app.enable('trust proxy');
 app.use(function(req, res, next){
         if(!req.secure){
                 res.redirect("https://"+ req.headers.host + req.url);
@@ -43,8 +26,8 @@ app.use(function(req, res, next){
         }
 });
 
-app.listen(8080);
-
-
+app.listen(PORT, () => {
+        console.log(`App listening at http://localhost:${PORT}`);
+}) 
 
 module.exports = app;
